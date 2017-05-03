631e1b47a5b9d08e0e40369691fdf9f0c5441028
Sarah Joy <sajoy.pc@gmail.com>
2017-05-03 11:38:49 -0700

    update correctanswers

--- class-03-lists-loops-box_model/demo/game/script.js
+++ class-03-lists-loops-box_model/demo/game/script.js
@@ -1,6 +1,6 @@
 // MVP of guessing game!
 
-var correctAnswers = [ 5, 13, 20, 7 ];
+var correctAnswers = [ 1, 5, 7, 13, 20, 64 ];
 var busLineGuess = prompt( 'What TriMet lines are down? There are multiple correct answers.' );
 
 // TODO check if busLineGuess equals a value in correctAnswers
